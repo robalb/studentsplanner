fd93cfbba4972dfa0a1937ac61bb3edb112a27d6
robalb <alberto.ventafridda@gmail.com>
2019-12-08 02:13:19 +0100

    wip on calendar: css structure

--- react-studentsplanner/src/components/Calendar.js
+++ react-studentsplanner/src/components/Calendar.js
@@ -1,12 +1,59 @@
-import React from 'react';
+
+// https://programmingwithmosh.com/react/build-a-react-calendar-component-from-scratch/
+import React from "react";
+import moment from "moment";
+
 import './calendar.css';
 
-function Calendar(props){
-  return(
-    <div>
-      <p> calendar </p>
-    </div>
-  );
-}
+export default class Calendar extends React.Component {
+  render() {
+    let weekdayshort = moment.weekdaysShort();
+    //europena -italian calendars have monday as the first day of the week
+    //this should fix the problem
+    //https://stackoverflow.com/questions/18875649/starting-the-week-on-monday-with-isoweekday
+    //this will return the first day of the month in the locale name format (not really relevant with the problem)
+    //new Date(2019,11,1).toLocaleDateString(undefined, { weekday: 'long' })
+    weekdayshort = [ 'lun', 'mar', 'mer', 'gio', 'ven', 'sab', 'dom' ];
 
-export default Calendar;
+    let weekdayshortname = weekdayshort.map(day => {
+      return (
+        <div key={day} >
+        {day}
+        </div>
+      );
+    });
+
+    return (
+      <div className="calendar-container">
+        <div className="month-panel">
+          <i className="material-icons"> arrow_left </i>
+          <div> <p>2019</p> </div>
+          <div> <p>gennaio</p> </div>
+          <i className="material-icons"> arrow_right </i>
+        </div>
+        <div className="weeks-labels">
+          {weekdayshortname}
+        </div>
+        <div className="calendar-grid">
+          <div className="first-el-test">1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+          <div>7</div>
+          <div>8</div>
+          <div>9</div>
+          <div>10</div>
+          <div>11</div>
+          <div>12</div>
+          <div>13</div>
+          <div>14</div>
+          <div>15</div>
+          
+        </div>
+        
+      </div>
+    );
+  }
+}
