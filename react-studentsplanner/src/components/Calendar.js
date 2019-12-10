d1e4539b387b5ccf6597412b6c7b6d97d52fd592
robalb <alberto.ventafridda@gmail.com>
2019-12-10 01:14:17 +0100

    cleaned files, added calendar month selection

--- react-studentsplanner/src/components/Calendar.js
+++ react-studentsplanner/src/components/Calendar.js
@@ -3,7 +3,6 @@
 import React from "react";
 // import moment from "moment";
 import moment from 'moment/min/moment-with-locales';
-import {getCountryIsoWeekDay} from '../utils/time.js';
 import dateFns from "date-fns";
 
 import './calendar.css';
@@ -18,20 +17,38 @@ export default class Calendar extends React.Component {
     this.state = {
       //the calendar language
       locale: locale,
-
       //TODO: optionally receive this as prop
-      currentMonth: moment()
+      currentMonth: moment(),
+      choosingMonth: false
     }
 
   }
 
   renderHeader(){
+    let previousMonth = ()=>{
+      let newMonth = this.state.currentMonth.clone().subtract(1, 'M');
+      this.setState({
+        currentMonth: newMonth
+      });
+    }
+    let nextMonth = ()=>{
+      let newMonth = this.state.currentMonth.clone().add(1, 'M');
+      this.setState({
+        currentMonth: newMonth
+      });
+    }
+    let toggleMonthSelection = ()=>{
+      this.setState({
+        choosingMonth: !this.state.choosingMonth
+      });
+    }
+
     return(
       <div className="header">
-        <i className="material-icons"> arrow_left </i>
-        <div> <p>{this.state.currentMonth.format("YYYY")}</p> </div>
-        <div> <p>{this.state.currentMonth.format("MMMM")}</p> </div>
-        <i className="material-icons"> arrow_right </i>
+        <i className="material-icons" onClick={previousMonth}> arrow_left </i>
+        <div > <p>{this.state.currentMonth.format("YYYY")}</p> </div>
+        <div onClick={toggleMonthSelection}> <p>{this.state.currentMonth.format("MMMM")}</p> </div>
+        <i className="material-icons" onClick={nextMonth}> arrow_right </i>
       </div>
     );
   }
@@ -71,13 +88,37 @@ export default class Calendar extends React.Component {
       </div>
     );
   }
+  renderMonthSelection(){
+    let monthsArray = moment.months(true);
+    let monthClick = m=>{
+      let newMonth = this.state.currentMonth.clone().month(m);
+      this.setState({
+        choosingMonth: false,
+        currentMonth: newMonth
+      });
+    }
+    let months = monthsArray.map(month => {
+      let current = (month == this.state.currentMonth.format('MMMM')) ?
+        'current' : '';
+      return (
+        <div className={current} onClick={()=>monthClick(month)} key={month} >
+        {month}
+        </div>
+      );
+    });
+    return <div className="month-selection">{ months }</div>;
+  }
 
   render() {
     return (
       <div className="calendar-container">
         {this.renderHeader()}
-        {this.renderDays()}
-        {this.renderCells()}
+
+        {this.state.choosingMonth?
+          this.renderMonthSelection()
+          :
+          [ this.renderDays(), this.renderCells() ]
+        }
       </div>
     );
   }
