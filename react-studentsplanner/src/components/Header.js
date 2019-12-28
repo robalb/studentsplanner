62d75f091d0ff2acc9d78f760b03349100db0e55
robalb <alberto.ventafridda@gmail.com>
2019-12-28 19:52:36 +0100

    wip on loader customization

--- react-studentsplanner/src/components/Header.js
+++ react-studentsplanner/src/components/Header.js
@@ -4,6 +4,12 @@ import accountContext from '../contexts/accountContext.js';
 import ReactPlaceholder from 'react-placeholder';
 import "react-placeholder/lib/reactPlaceholder.css";
 
+//TODO: move to a HOC, in a separate folder
+import {TextRow} from 'react-placeholder/lib/placeholders';
+const awesomePlaceholder = (
+    <TextRow color='#E0E0E0' style={{margin: 0}}/>
+);
+
 function Header(props){
   // let account = React.useContext(accountContext);
   // let isAdmin = !account.loading && account.data.user.isAdmin;
@@ -18,12 +24,12 @@ function Header(props){
         </div>
       </div>
       <div className="class-info"> 
-        <ReactPlaceholder ready={!loading} type={"textRow"} firstLaunchOnly={true} showLoadingAnimation={true}>
+        <ReactPlaceholder ready={!loading} type={"textRow"} customPlaceholder={awesomePlaceholder} firstLaunchOnly={true} showLoadingAnimation={true}>
           <h2 className="name">{data.classroomName}</h2>
        </ReactPlaceholder>
       </div>
       <div className="user">
-        <ReactPlaceholder ready={!loading} type={"textRow"} firstLaunchOnly={true} showLoadingAnimation={true}>
+        <ReactPlaceholder ready={!loading} type={"textRow"} color='#E0E0E0' firstLaunchOnly={true} showLoadingAnimation={true}>
           <p className="name">{!loading && data.user.uid}</p>
           {!loading && data.user.isAdmin? <p className="status-badge">admin</p> : ''}
         </ReactPlaceholder>
