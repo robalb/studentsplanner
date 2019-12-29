5570b0de11579f098840282c1fcf6a8393f940b1
robalb <alberto.ventafridda@gmail.com>
2019-12-29 19:58:04 +0100

    adapted all cards to state system, added preloader to eventlistcard, wip on eventilstcard creationmode

---
+++ react-studentsplanner/src/planner/EventsListCard/CreationMenu.js
@@ -0,0 +1,36 @@
+
+//function handleEventCreationBtn(){
+//  //TODO
+//  //spawn a popup that allows the insertion of all the data.
+//  //on confirm, the popup will close, and call plannerContext.update
+
+//  //the real popup should do somethig similar to this, but:
+//  //choose colors that arent used first, and when they finish create them by mixing existing basecolors
+//  let randomColorIndex = Math.floor(Math.random()*colors.colorsList.length)
+//  let randomColor = colors.colorsList[randomColorIndex]
+//  const testEvent = {
+//    name: "sample event"+Math.floor(Math.random()*999),
+//    repeatStudents: false,
+//    baseColor: randomColor,
+//    dates: [
+//      {
+//        day: 10,
+//        month: 11,
+//        students: []
+//      },
+//      {
+//        day: 13,
+//        month: 11,
+//        students: []
+//      }
+//    ]
+//  };
+//}
+
+import React from 'react';
+
+function CreationMenu(){
+  return <p>creation menu</p>
+}
+
+export default CreationMenu;
