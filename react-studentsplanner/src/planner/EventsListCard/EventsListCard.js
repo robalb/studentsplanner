969edb0658e7fdabca74423e1b0456a02b5f12d7
robalb <alberto.ventafridda@gmail.com>
2020-01-11 01:22:05 +0100

    small tweaks to eventslistcard controls

--- react-studentsplanner/src/planner/EventsListCard/EventsListCard.js
+++ react-studentsplanner/src/planner/EventsListCard/EventsListCard.js
@@ -9,33 +9,48 @@ function EventsListCard(){
   const [creationMode, setCreationMode] = React.useState(true);
 
   function toggleViewMode(){
-    if(creationMode){
-      setCreationMode(false);
-    }else{
-      setViewMode(current => current == 'list' ? 'calendar' : 'list');
-    }
+    setViewMode(current => current == 'list' ? 'calendar' : 'list');
   }
   
+  //TODO: refactor and clean this mess into this: 
+  //topBarContent = [title] [controls]
+  //view = ...
+  //if creationMode {
+  //topBarcontent = [title] [controls]
+  //view = ..
+  //}
   let toggleIcon = viewMode == 'list' ? 'event' : 'view_list';
   let view = viewMode == 'list' ?  <ListView setCreationMode={setCreationMode}/>: <CalendarView/>
   if(creationMode) view = <CreationMenu setCreationMode={setCreationMode}/>
+
+  let controls = creationMode ?
+    (
+      <button aria-label={"undo event ceration"}
+     title={"undo event creation"}
+     onClick={()=>setCreationMode(false)}
+     ><i className="material-icons">close</i></button>
+    )
+       :
+    (
+      <div>
+      <button aria-label={"toggle view mode"}
+      title={"toggle view mode"}
+      onClick={toggleViewMode}>
+      <i className="material-icons">{toggleIcon}</i>
+      </button>
+      <button
+      aria-label={"events options"}
+      title={"events options"}
+      >
+      <i className="material-icons">more_vert</i>
+      </button>
+      </div>
+  )
   return (
     <div className="card events-list">
       <div className="top-bar">
         <h2>{creationMode ? 'create event' : 'events' }</h2>
-        <div>
-          <button aria-label={"toggle view mode"}
-          title={"toggle view mode"}
-          onClick={toggleViewMode}>
-           <i className="material-icons">{toggleIcon}</i>
-          </button>
-          <button
-          aria-label={"events options"}
-          title={"events options"}
-          >
-           <i className="material-icons">more_vert</i>
-          </button>
-        </div>
+        {controls}
       </div>
       {view}
     </div>
