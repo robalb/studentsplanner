045b4927a020f41f42b1f3df279e1be89fc5e61e
robalb <alberto.ventafridda@gmail.com>
2020-01-12 21:14:07 +0100

    added loading bar, logic for the newEvent case in updateplannerdata

--- react-studentsplanner/src/planner/Planner.js
+++ react-studentsplanner/src/planner/Planner.js
@@ -1,6 +1,7 @@
 import React from 'react';
 //common imports
 import Header from '../components/Header.js';
+import LoadingBar from '../components/LoadingBar.js'
 //page specific imports
 import EventsListCard from './EventsListCard/EventsListCard';
 import EventCard from './EventCard';
@@ -16,7 +17,11 @@ class Planner extends React.Component{
     this.state = {
       accountDataLoading: true,
       accountData: {},
+      //if true, the app stays on hold, avoiding any logic involving plannerData, 
+      //and displaying placeholders
       plannerDataLoading: true,
+      //if true, the loading bar on top is displayed
+      plannerDataUpdating: false,
       plannerData: {},
       //the index of the currently selected event. all the events are in an array in plannerData
       currentEvent: undefined
@@ -51,6 +56,17 @@ class Planner extends React.Component{
     //remove loading bar
     switch(operation){
       case 'newEvent':
+        this.setState({
+           plannerDataUpdating: true,
+          plannerData: {
+            ...this.state.plannerData,
+            events: [...this.state.plannerData.events, newData]
+          }
+        })
+        const response = await getApiData('simulateOk')
+        this.setState({
+           plannerDataUpdating: false
+        })
         //TODO
         break;
       case 'updateEventDates':
@@ -99,6 +115,7 @@ class Planner extends React.Component{
     return (
       <accountContext.Provider value={{data: this.state.accountData, loading: this.state.accountDataLoading}}>
       <plannerContext.Provider value={plannerContextValues}>
+        <LoadingBar active={this.state.plannerDataUpdating}/>
         <Header/>
         <div className="content">
           <EventsListCard/>
