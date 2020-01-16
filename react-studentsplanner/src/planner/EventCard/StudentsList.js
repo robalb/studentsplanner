cb8c7ad9a61ef5078705aede7885082862616745
robalb <alberto.ventafridda@gmail.com>
2020-01-16 19:58:28 +0100

    added editDatestudents reducer handler, and wip on students picker

---
+++ react-studentsplanner/src/planner/EventCard/StudentsList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import plannerContext from '../../contexts/plannerContext.js';
+
+function StudentsList(props){
+  //TODO:
+  //show only if props.targetSelection is not false,
+  //add close button that  calls props.setSelectMode(false)
+  //add styles in planner.css
+  return (<a>studentsList</a>)
+}
+
+export default StudentsList;
