a67b4c39746c6237dfaa0a15121acc81a5b589cb
robalb <alberto.ventafridda@gmail.com>
2020-02-13 20:43:09 +0100

    added babel presets, wip on frontend webpack structure

--- pinboards.it/webpack.dev.js
+++ pinboards.it/webpack.dev.js
@@ -58,6 +58,16 @@ module.exports = {
           'css-loader',
         ],
       },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
     ],
   },
 
