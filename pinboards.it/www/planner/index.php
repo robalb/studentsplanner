ae951a1af13104c2dba2a0e1c7bcf631d8d57249
robalb <alberto.ventafridda@gmail.com>
2020-02-20 00:43:10 +0100

    ported neutrino experiments to main work folder

--- pinboards.it/www/planner/index.php
+++ pinboards.it/www/planner/index.php
@@ -2,9 +2,12 @@
 require_once '../core/classes/BundlesManager.php';
 $bundlesManager = new BundlesManager('planner', '../bundles/');
 ?>
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
   <head>
+    <meta charset="utf-8" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400|Material+Icons" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>planner</title>
   </head>
   <body>
