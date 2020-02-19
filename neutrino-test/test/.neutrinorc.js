9684d8e9a1575fd690cec5eb4ad6f08c010f5e4a
robalb <alberto.ventafridda@gmail.com>
2020-02-19 23:29:35 +0100

    fixed css imports and sourcemaps. added error overlay

--- neutrino-test/test/.neutrinorc.js
+++ neutrino-test/test/.neutrinorc.js
@@ -1,4 +1,6 @@
 const react = require('@neutrinojs/react');
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { StatsWriterPlugin } = require("webpack-stats-plugin");
 
 module.exports = {
@@ -16,6 +18,8 @@ module.exports = {
       //https://neutrinojs.org/packages/web/#deployment-path
       publicPath: '',
       html: false,
+      style: false,
+      clean: true,
       devServer: {
         port: 5000,
         open: false,
@@ -42,18 +46,42 @@ module.exports = {
             entrypoints: true,
           }
         }]);
+      //css loaders
+      neutrino.config.module
+        .rule('css')
+        .test(/\.css$/)
+          .use('style')
+            .loader('style-loader')
+            .end()
+          .use('css')
+            .loader('css-loader');
+      //conditional configs
       neutrino.config
         .when(process.env.NODE_ENV === 'production',
+          //PRODUCTION
           //fix bundles output path
           config => config.output.filename('[name].[contenthash:8].js'),
+          //DEVELOPMENT
+          config => config
+            .output
+              .filename('[name].js')
+              .end()
+            .plugin('clean')
+              .use(CleanWebpackPlugin)
+              .end()
+            .plugin('error-overlay')
+              .use(ErrorOverlayPlugin)
+              .end()
+            .devtool('inline-source-map')
         );
-      neutrino.config
-        .when(process.env.NODE_ENV === 'development',
-          //fix bundles output path
-          config => config.output.filename('[name].js'),
-          //devtools
-          config => config.devtool('eval-source-map'),
-        );
+      //neutrino.config
+      //  .when(process.env.NODE_ENV === 'development',
+      //    //fix bundles output path
+      //    //clean plugin also in development mode
+      //    config => config.plugin('clean').use(CleanWebpackPlugin),
+      //    //devtools
+      //    config => config.devtool('eval-source-map'),
+      //  );
 
     },
 
