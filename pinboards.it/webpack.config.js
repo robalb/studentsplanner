b5ffc50b9b35f8e99c85635797f198cfa0ec5bef
robalb <alberto.ventafridda@gmail.com>
2020-02-20 00:43:10 +0100

    ported neutrino experiments to main work folder

--- pinboards.it/webpack.config.js
+++ pinboards.it/webpack.config.js
@@ -1,9 +1,6 @@
+// Whilst the configuration object can be modified here, the recommended way of making
+// changes is via the presets' options or Neutrino's API in `.neutrinorc.js` instead.
+// Neutrino's inspect feature can be used to view/export the generated configuration.
+const neutrino = require('neutrino');
 
-//https://webpack.js.org/configuration
-//https://atendesigngroup.com/blog/managing-dev-and-production-builds-webpack
-
-
-module.exports =(env) => {
-  if(!env) env = 'dev';
-  return require(`./webpack.${env}.js`);
-}
+module.exports = neutrino().webpack();
