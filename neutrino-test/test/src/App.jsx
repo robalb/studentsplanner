01833ef222fe493d36d737d601f50c4de0aa88f2
robalb <alberto.ventafridda@gmail.com>
2020-02-19 22:23:44 +0100

    wip on neutrino config. trying to fix css import, and sourcemaps

--- neutrino-test/test/src/App.jsx
+++ neutrino-test/test/src/App.jsx
@@ -1,12 +1,11 @@
-import { hot } from 'react-hot-loader';
 import React from 'react';
-import './App.css';
+// import './App.css';
 
-const message = 'Welcome to test 6';
+const message = 'Welcome to test 1';
 const App = () => (
   <div className="App">
     <h1>{message}</h1>
   </div>
 );
 
-export default hot(module)(App);
+export default App;
