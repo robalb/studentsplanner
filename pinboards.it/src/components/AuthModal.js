88f86835181bbe7aa1078a931e1fef7123e99bf1
robalb <alberto.ventafridda@gmail.com>
2020-02-26 01:35:01 +0100

    completed sign in modal css. wip on modal logic

--- pinboards.it/src/components/AuthModal.js
+++ pinboards.it/src/components/AuthModal.js
@@ -1,12 +1,34 @@
 import React from 'react';
 
 function AuthModal(props){
+  //TODO: internal data validation. if valid, call received callback on click.
   return(
     <div class="auth-modal-wrapper">
       <div class="auth-modal">
-      <h2>sign in</h2>
-      <p>mail</p>
-      <p>password</p>
+      <h2>please sign in to continue</h2>
+
+      <div className="input-group centered">
+        <input id="a11y-input3"
+        aria-labelledby="#a11y-input3"
+        type="mail"
+        required/>
+        <span className="bar"></span>
+        <label htmlFor="a11y-input3">Mail</label>
+      </div>
+
+      <div className="input-group centered">
+        <input id="a11y-input2"
+        aria-labelledby="#a11y-input2"
+        type="password"
+        required/>
+        <span className="bar"></span>
+        <label htmlFor="a11y-input2">Password</label>
+      </div>
+
+        <button className="btn"
+        aria-label={"sign in"}
+        >sign in</button>
+      
       </div>
     </div>
   );
