0e036343fe1c2b426b36793f1548cfe8e26fb296
robalb <alberto.ventafridda@gmail.com>
2020-03-26 16:08:47 +0100

    cleaned some comments

--- pinboards.it/src/utils/apiResolver.js
+++ pinboards.it/src/utils/apiResolver.js
@@ -5,8 +5,6 @@ const API_PATH = (envBasePath || '') + '/apis/';
 
 //get the csrf token
 let CSRF = 'no_csrf_found';
-//TODO: decomment this once the csrf token injection has been
-//implemented in php
 if(PHP_CSRF){
   CSRF = PHP_CSRF;
 }
@@ -35,47 +33,7 @@ export async function apiRequest(api, data, method){
   return decoded;
 }
 
-//export async function getApiData(elementsArray)
-//{
-//  let data = {
-//    elements: elementsArray
-//  };
-//  return await apiRequest('getData', data, 'GET');
-//}
-
-//export async function updatePlannerData(updateData){
-//  let data = {
-//    action: 'push',
-//    data: updateData
-//  }
-//  return await apiRequest('planner', data, 'GET');
-//}
-
-//export async function register(registrationData){
-//  return await apiRequest('register', registrationData, 'POST');
-//}
-
-
-//export async function login(mail, password, isHash, getData){
-//  return await apiRequest('login', registrationData, 'POST');
-//  let response = await fetch(`${API_PATH}login.php`, {
-//    headers: { "Content-Type": "application/json; charset=utf-8" },
-//    method: 'POST',
-//    body: JSON.stringify({
-//      mail: mail,
-//      password: password,
-//      isHash: isHash,
-//      getData: getData
-//    })
-//  });
-//  let data = await response.json();
-//  return data;
-//}
-
 ////for debugging purposes
 //function sleep(ms) {
 //  return new Promise(resolve => setTimeout(resolve, ms));
 //}
-
-//// getApiData('yourUsernameHere')
-////   .then(data => console.log(data));
