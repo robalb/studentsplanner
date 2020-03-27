7d5d4ebfa5c54c9c389cbb320dab5369ed02c625
robalb <alberto.ventafridda@gmail.com>
2020-03-27 22:48:16 +0100

    refactoring header: improving ux, adding navigation and i18n!

--- pinboards.it/src/components/Header.js
+++ pinboards.it/src/components/Header.js
@@ -1,30 +1,41 @@
 import React from 'react';
 // TODO: implement svg loaders
-import logo from '../assets/logo.svg';
+// import logo from '../assets/logo.svg';
 import accountContext from '../contexts/accountContext.js';
 import PlaceHolder from './PlaceHolder.js';
 
+import Button from './Button.js';
+import t from '../utils/i18n.js';
+
 function Header(props){
   let {data, loading} = React.useContext(accountContext);
+  let pages = [ 'account', 'planner', 'polls' ];
+  let currentPage = props.currentPage ?? pages[0];
+  let list = pages.filter(page => page != currentPage).map( page => <a href={'../' + page}>{t(page)}</a> )
   return(
     <header>
-      <div className="logo">
-        <div className="temporary-swg"> 
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
+    <div className="menu-container">
+      <div className="burger">
+        <a href="#">{t(currentPage)}</a>
+        <span class="material-icons"> arrow_drop_down </span>
       </div>
+      <div className="menu">
+        {list}
+      </div>
+    </div>
+
       <div className="class-info"> 
         <PlaceHolder ready={!loading}>
           <h2 className="name">{data.classroomName}</h2>
        </PlaceHolder>
       </div>
-      <div className="user">
+
+      <Button className="user">
         <PlaceHolder ready={!loading}>
           <p className="name">{!loading && data.user.uid}</p>
-          {!loading && data.user.isAdmin? <p className="status-badge">admin</p> : ''}
         </PlaceHolder>
         <i className="material-icons"> account_circle </i>
-      </div>
+      </Button>
     </header>
   );
 }
