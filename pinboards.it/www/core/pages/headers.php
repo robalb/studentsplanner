4f5b2227afbd01b35e746760e8259b7ce47667c4
robalb <alberto.ventafridda@gmail.com>
2020-04-21 22:12:17 +0200

    working on common headers

---
+++ pinboards.it/www/core/pages/headers.php
@@ -0,0 +1,25 @@
+<?php
+  
+?>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400|Material+Icons&display=swap" rel="stylesheet">
+<meta name="description" content="pinboards, l'app di pianificazione scolastica" />
+<meta name="keywords" content="pinboards, scuola, pianificazione, sondaggi" />
+<meta name="language" content="it_IT">
+
+<meta name="og:title" property="og:title" content="pinboards, l'app di pianificazione scolastica" />
+<meta name="og:description" property="og:description" content="organizza la tua classe, crea sondaggi o pianifica le interrogazioni" />
+<meta name="og:type" property="og:type" content="website" />
+<meta name="og:locale" property="og:locale" content="it_IT" />
+<!-- insert a tag of this type for every language supported -->
+<meta name="og:locale:alternate" property="og:locale:alternate" content="fr_FR" />
+<meta name="og:locale:alternate" property="og:locale:alternate" content="en_US" />
+
+<meta name="og:site_name" property="og:type" content="pinboards" />
+<meta name="og:image:alt" property="og:image:alt" content="logo" />
+<meta name="og:image:type" property="og:image:type" content="image/png" />
+<meta name="og:image:width" property="og:image:width" content="400" />
+<meta name="og:image:height" property="og:image:height" content="300" />
+<meta name="og:image" property="og:image" content="http://path-to-png" />
