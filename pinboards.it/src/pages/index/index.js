ed0ba875cbbaff5fccb9405748e5fcbc2e8da249
robalb <alberto.ventafridda@gmail.com>
2020-04-22 00:26:05 +0200

    cleaned index code

--- pinboards.it/src/pages/index/index.js
+++ pinboards.it/src/pages/index/index.js
@@ -1,21 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+/*
+ * the index page is in plain html and css, no React
+ */
 import '../../styles/sharedStyles.css';
-
-// function Index(){
-//   return (
-//     <>
-//     <h1>Pinboards</h1><br/>
-//     <p>homepage</p>
-//     <p><a href="./account">accedi</a><br/> </p>
-//     <p> <a href="./register"> registrati </a></p>
-//       <p>this is project is open source at <a href="https://github.com/robalb/studentsplanner/">github</a> </p>
-//     </>
-//   )
-// }
-// // import Index from './Index.js';
-
 import cssGlobal from '../../utils/cssFocusHelper.js';
-
-// ReactDOM.render(<Index />, document.getElementById('root'));
-
