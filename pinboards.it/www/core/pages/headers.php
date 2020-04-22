a0f75724f5a90c8b7d4312d5571f32dbf2e4abfd
robalb <alberto.ventafridda@gmail.com>
2020-04-22 11:22:27 +0200

    tweaks to the meta tags

--- pinboards.it/www/core/pages/headers.php
+++ pinboards.it/www/core/pages/headers.php
@@ -1,24 +1,7 @@
-<?php
-  
-?>
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 <link href="https://fonts.googleapis.com/css?family=Roboto:300,400|Material+Icons&display=swap" rel="stylesheet">
 <meta name="description" content="<?php $t('description');?>" />
-<meta name="keywords" content="<?php $t('keywords');?>" />
 
-<meta name="og:title" property="og:title" content="<?php $t('og title');?>" />
-<meta name="og:description" property="og:description" content="<?php $t('og description');?>" />
-<meta name="og:type" property="og:type" content="website" />
-<meta name="og:locale" property="og:locale" content="<?php $t('og locale');?>" />
-<!-- insert a tag of this type for every language supported -->
-<meta name="og:locale:alternate" property="og:locale:alternate" content="it_IT" />
-<meta name="og:locale:alternate" property="og:locale:alternate" content="en_US" />
-
-<meta name="og:site_name" property="og:type" content="pinboards" />
-<meta name="og:image:alt" property="og:image:alt" content="logo" />
-<meta name="og:image:type" property="og:image:type" content="image/png" />
-<meta name="og:image:width" property="og:image:width" content="400" />
-<meta name="og:image:height" property="og:image:height" content="300" />
-<!--<meta name="og:image" property="og:image" content="http://path-to-png" /> -->
--- pinboards.it/www/index.php
+++ pinboards.it/www/index.php
@@ -6,7 +6,6 @@ require_once './core/classes/ConnectDb.php';
 require_once './core/classes/LanguageManager.php';
 
 $bundlesManager = new BundlesManager('index', './bundles/');
-$nonce = SecurityHeaders::getNonce();
 $session = new SessionManager();
 $isLogged = $session->isValid();
 
@@ -24,8 +23,32 @@ $t = $languageManager->getT();
 <html lang="<?php echo $locale;?>">
   <head>
     <meta name="language" content="<?php echo $locale;?>">
+    <title><?php $t('register');?></title>
+    <link rel="canonical" href="https://www.pinboards.it/" />
+
     <?php include './core/pages/headers.php';?>
-    <title><?php $t('index');?></title>
+
+    <meta name="keywords" content="<?php $t('keywords');?>" />
+    <meta name="og:title" property="og:title" content="<?php $t('og title');?>" />
+    <meta name="og:description" property="og:description" content="<?php $t('og description');?>" />
+    <meta name="og:site_name" property="og:site_name" content="Pinboards" />
+    <meta name="og:type" property="og:type" content="website" />
+    <meta name="og:locale" property="og:locale" content="<?php $t('og locale');?>" />
+    <!-- insert a tag of this type for every language supported -->
+    <meta name="og:locale:alternate" property="og:locale:alternate" content="it_IT" />
+    <meta name="og:locale:alternate" property="og:locale:alternate" content="en_US" />
+    <!--<meta name="og:image" property="og:image" content="http://path-to-png" />
+    <meta name="og:image:alt" property="og:image:alt" content="logo" />
+    <meta name="og:image:type" property="og:image:type" content="image/png" />
+    <meta name="og:image:width" property="og:image:width" content="400" />
+    <meta name="og:image:height" property="og:image:height" content="300" />-->
+    <meta property="al:ios:app_name" content="Pinboards" />
+    <meta property="al:ios:app_store_id" content="" />
+    <meta property="al:ios:url" content="" />
+    <meta property="al:android:app_name" content="Pinboards" />
+    <meta property="al:android:package" content="" />
+    <meta property="al:android:url" content="" />
+
     <?php $bundlesManager->headOutput();?>
   </head>
   <body>
