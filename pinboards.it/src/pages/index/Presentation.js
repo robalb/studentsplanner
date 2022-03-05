2c9180a901dc16e607f069da1a059fe8ff93b676
robalb <alberto.ventafridda@gmail.com>
2022-03-06 00:37:07 +0100

    added polls info section

--- pinboards.it/src/pages/index/Presentation.js
+++ pinboards.it/src/pages/index/Presentation.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import a from './assets/v2_full.png'
 import p1 from './assets/preview_1.png'
+import p2 from './assets/preview_2.png'
+import b1 from './assets/blur_1.png'
 
 export default function Presentation(props){
     return(
@@ -34,24 +36,28 @@ export default function Presentation(props){
                 <img src={p1}></img>
             </div>
             <div className="preview_1">
-                <img src={p1}></img>
+                <img src={p2}></img>
                 <section>
                     <h1>crea sondaggi</h1>
-                    <p><b>Pianifica i calendari delle interrogazioni </b>
-                    assegnando un colore ad ogni materia</p>
-                    <p><b>assegna gli studenti ad un giorno.</b> L'app suggerirà 
-                    gli studenti più liberi, e colorerà il nome degli studenti impegnati
-                    con il colore dell'esame più incombente.  </p>
-                    <p><b>Estrai a sorte</b> uno
-                    studente tra quelli più liberi da esami, oppure organizza
-                    con un click l'intero calendario nella maniera più efficente
-                    possibile 
-                    grazie al potente algoritmo messo a disposizione dalla piattaforma. </p>
+                    <p><b>organizza votazioni o sondaggi </b>
+                    scegliendo tra un'ampia gamma di formati disponibili.</p>
+                    <p><b>Evita doppi voti o brogli; </b> 
+                    Solo i membri della classe potranno votare, senza possibilità
+                    di ambiguità.
+                    </p>
+                    <p><b>Sfrutta gli strumenti avanzati </b>
+                    di votazione anonima, trasparenza dei voti e molto altro
+                    per raggiungere una decisione nella maniera più adatta
+                    all'esigenza della classe.
+                    </p>
                     
                 </section>
             </div>
-            <p>Organizza votazioni o sondaggi a cui potranno partecipare 
-                solo i membri della tua classe.</p>
+            <div className="start_now">
+                <p>Pinboards è una piattaforma gratuita e completamente open source</p>
+                <a href="./register">registrati</a>
+                <img src={b1}></img>
+            </div>
 
         </>
     )
