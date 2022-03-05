a8b3e3e49fb28549a9a6066ce2c47e9084d704a3
robalb <alberto.ventafridda@gmail.com>
2022-03-05 20:37:58 +0100

    added presentation

--- pinboards.it/src/pages/index/Presentation.js
+++ pinboards.it/src/pages/index/Presentation.js
@@ -1,23 +1,57 @@
 import React from 'react';
 
 import a from './assets/v2_full.png'
+import p1 from './assets/preview_1.png'
 
 export default function Presentation(props){
     return(
         <>
-        <header>
-            <a href="./register">registrati</a>
-            <a className="secondary" href="./account">accedi</a>
-        </header>
-        <div className="hero">
-        <h1>Pinboards</h1>
-        <h2>A highschool management platform for students</h2>
-            <img className="hero" src={a} />
-        </div>
-   <p><a href="./account">accedi</a><br/> </p>
-   <p> <a href="./register"> registrati </a></p>
-   <p>this is project is open source at <a href="https://github.com/robalb/studentsplanner/">github</a> </p>
-   <p>aasd</p>
+            <header>
+                <a href="./register">registrati</a>
+                <a className="secondary" href="./account">accedi</a>
+            </header>
+            <div className="hero">
+                <h1>Pinboards</h1>
+                <h2>A management platform for highschool students</h2>
+                <img className="hero" src={a} />
+            </div>
+
+            <div className="preview_1">
+                <section>
+                    <h1>Pianifica interrogazioni</h1>
+                    <p><b>Pianifica i calendari delle interrogazioni </b>
+                    assegnando un colore ad ogni materia</p>
+                    <p><b>assegna gli studenti ad un giorno.</b> L'app suggerirà 
+                    gli studenti più liberi, e colorerà il nome degli studenti impegnati
+                    con il colore dell'esame più incombente.  </p>
+                    <p><b>Estrai a sorte</b> uno
+                    studente tra quelli più liberi da esami, oppure organizza
+                    con un click l'intero calendario nella maniera più efficente
+                    possibile 
+                    grazie al potente algoritmo messo a disposizione dalla piattaforma. </p>
+                    
+                </section>
+                <img src={p1}></img>
+            </div>
+            <div className="preview_1">
+                <img src={p1}></img>
+                <section>
+                    <h1>crea sondaggi</h1>
+                    <p><b>Pianifica i calendari delle interrogazioni </b>
+                    assegnando un colore ad ogni materia</p>
+                    <p><b>assegna gli studenti ad un giorno.</b> L'app suggerirà 
+                    gli studenti più liberi, e colorerà il nome degli studenti impegnati
+                    con il colore dell'esame più incombente.  </p>
+                    <p><b>Estrai a sorte</b> uno
+                    studente tra quelli più liberi da esami, oppure organizza
+                    con un click l'intero calendario nella maniera più efficente
+                    possibile 
+                    grazie al potente algoritmo messo a disposizione dalla piattaforma. </p>
+                    
+                </section>
+            </div>
+            <p>Organizza votazioni o sondaggi a cui potranno partecipare 
+                solo i membri della tua classe.</p>
 
         </>
     )
