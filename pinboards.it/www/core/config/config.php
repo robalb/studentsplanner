551dcaf1179cee68ed465a89b02d6d141cd0a3e5
alberto ventafridda <alberto.ventafridda@gmail.com>
2022-03-08 01:01:43 +0100

    Update config.php

--- pinboards.it/www/core/config/config.php
+++ pinboards.it/www/core/config/config.php
@@ -1,13 +1,5 @@
 <?php
 
-/**
- * CONFIGURATION VARIABLES
- * ATTENZIONE: non includere modifiche con informazioni sensibili in futuri commit
- * è possibile configurare git per ignorare automaticamente qualsiasi modifica locale al file.
- * Per farlo, dopo essersi assicurati che app/config.php è il percorso corretto di questo file, scrivere:
- * git update-index --assume-unchanged app/config.php
- *
- */
 return array(
   //DATABASE CONFIGURATION
   'dbHost' => getenv('MARIADB_HOST'),
